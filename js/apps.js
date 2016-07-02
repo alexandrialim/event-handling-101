5f51dc3097c2ac2c7aa776b96f16535b991fa24b
Student 10AM <student10@js-c02mxmy3g083.punahou.edu>
2016-07-01 15:02:03 -1000

    apps inprogress

--- js/apps.js
+++ js/apps.js
@@ -1,4 +1,8 @@
 window.onload = doThings;
 function doThings(){
-	
+	var lanikai = document.getElementById( "lanikai" ); 
+	lanikai.addEventListener("click", handleclick);
+}
+function handleclick(evt){
+console.log(evt.target.id);
 }
